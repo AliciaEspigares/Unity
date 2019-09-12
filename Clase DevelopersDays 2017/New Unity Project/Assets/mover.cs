3fc34091078e1d93861a76dfeff25db9d3ab9449
AliciaEspigares <alicia.espigares@goumh.umh.es>
2019-09-12 17:51:22 +0200

    Upload Unity course

---
+++ Clase DevelopersDays 2017/New Unity Project/Assets/mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+    public int speedrotate;
+    public int speedmove;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(1,0,0)*Time.deltaTime*speedmove,Space.World);
+        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime*speedrotate);
+        
+
+		
+	}
+}
